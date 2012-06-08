e743dcbdafa3f0c98307850a00374fd831479685
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-06-08 08:12:28 +0200

    Reindentation and stale files removal

--- imaging/tests.py
+++ imaging/tests.py
@@ -8,50 +8,50 @@ import stat
 import sys
 
 try:
-  IMAGING_SETTINGS = settings.IMAGING_SETTINGS
+    IMAGING_SETTINGS = settings.IMAGING_SETTINGS
 except:
-  IMAGING_SETTINGS = DEFAULT_IMAGING_SETTINGS
+    IMAGING_SETTINGS = DEFAULT_IMAGING_SETTINGS
 
 PERMS = "777"
 
 class TestImagingSetup(TestCase):
-  def test_file_permissions(self):
-    path = settings.MEDIA_ROOT+"/"+IMAGING_SETTINGS['image_dir']
-    filemode = stat.S_IMODE(os.stat(path).st_mode)
-    self.assertEquals(filemode, int(PERMS, 8))
+    def test_file_permissions(self):
+        path = settings.MEDIA_ROOT+"/"+IMAGING_SETTINGS['image_dir']
+        filemode = stat.S_IMODE(os.stat(path).st_mode)
+        self.assertEquals(filemode, int(PERMS, 8))
 
 class TestAjax(TestCase):
-  urls = 'imaging.urls'
-  fixtures = ['users.json']
+    urls = 'imaging.urls'
+    fixtures = ['users.json']
 
-  def setUp(self):
-    self.is_logged = self.client.login(username="admin", password="nimda")
-    self.upload_path = settings.MEDIA_ROOT+"/"+IMAGING_SETTINGS['image_dir']
+    def setUp(self):
+        self.is_logged = self.client.login(username="admin", password="nimda")
+        self.upload_path = settings.MEDIA_ROOT+"/"+IMAGING_SETTINGS['image_dir']
 
-  def test_image_upload(self):
-    testfile = open(settings.MEDIA_ROOT+"/imaging/imaging_testfile.jpg")
-    data = {
-        'name': 'Test Image: guilt',
-        'alt': 'Alt of guilt',
-        'title': 'Title of guilt',
-        'image': testfile,
-        }
-    response = self.client.post(reverse('imaging_iframe_form'), data, follow=True)
-    testfile.close()
+    def test_image_upload(self):
+        testfile = open(settings.MEDIA_ROOT+"/imaging/imaging_testfile.jpg")
+        data = {
+                'name': 'Test Image: guilt',
+                'alt': 'Alt of guilt',
+                'title': 'Title of guilt',
+                'image': testfile,
+                }
+        response = self.client.post(reverse('imaging_iframe_form'), data, follow=True)
+        testfile.close()
 
-    # clean up test uploads
-    uploaded = os.remove(self.upload_path+"/imaging_testfile.jpg")
-    uploaded = os.remove(self.upload_path+"/imaging_thumbnail/imaging_testfile.jpg")
-    uploaded = os.remove(self.upload_path+"/small_thumbnail/imaging_testfile.jpg")
+        # clean up test uploads
+        uploaded = os.remove(self.upload_path+"/imaging_testfile.jpg")
+        uploaded = os.remove(self.upload_path+"/imaging_thumbnail/imaging_testfile.jpg")
+        uploaded = os.remove(self.upload_path+"/small_thumbnail/imaging_testfile.jpg")
 
-    self.assertEquals(self.is_logged, True)
-    self.assertEquals(response.status_code, 200)
-    self.assertNotEqual(response.context['callback'], None)
+        self.assertEquals(self.is_logged, True)
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(response.context['callback'], None)
 
 #TODO: figoure out why there is no new image
 # def test_image_removal(self):
-#   data = {
-#       'id': 1
-#       }
-#   response = self.client.post(reverse('imaging_image_removal'), data, follow=True)
-#   self.assertContains(response, 'ok', status_code=200)
+#     data = {
+#             'id': 1
+#             }
+#     response = self.client.post(reverse('imaging_image_removal'), data, follow=True)
+#     self.assertContains(response, 'ok', status_code=200)
--- imaging/views.py
+++ imaging/views.py
@@ -7,48 +7,46 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.utils import simplejson
 from django.core.files import File
 
-# Create your views here.
-
 def iframe_form(request):
-  if request.method == 'POST':
-    form = AjaxUploadForm(request.POST, request.FILES)
-    if form.is_valid():
-      new_image = form.save()
-      response_dict = { # data that will be serialized by json
-          'title':form.cleaned_data['title'],
-          'alt'  :form.cleaned_data['alt'],
-          'name' :form.cleaned_data['name'][:25],
-          'id'   :new_image.pk,
-          'image':new_image.imaging_thumbnail.url,
-          }
-      form = AjaxUploadForm()
-      return render_to_response('imaging/iframe_form.html',
-          { 'form' : form, 'callback': simplejson.dumps(response_dict) },
-          context_instance=RequestContext(request))
+    if request.method == 'POST':
+        form = AjaxUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_image = form.save()
+            response_dict = { # data that will be serialized by json
+                    'title':form.cleaned_data['title'],
+                    'alt'    :form.cleaned_data['alt'],
+                    'name' :form.cleaned_data['name'][:25],
+                    'id'     :new_image.pk,
+                    'image':new_image.imaging_thumbnail.url,
+                    }
+            form = AjaxUploadForm()
+            return render_to_response('imaging/iframe_form.html',
+                    { 'form' : form, 'callback': simplejson.dumps(response_dict) },
+                    context_instance=RequestContext(request))
+        else:
+            return render_to_response('imaging/iframe_form.html',
+                    { 'form' : form },
+                    context_instance=RequestContext(request))
     else:
-      return render_to_response('imaging/iframe_form.html',
-          { 'form' : form },
-          context_instance=RequestContext(request))
-  else:
-    form = AjaxUploadForm()
-    return render_to_response('imaging/iframe_form.html',
-        { 'form' : form },
-        context_instance=RequestContext(request))
+        form = AjaxUploadForm()
+        return render_to_response('imaging/iframe_form.html',
+                { 'form' : form },
+                context_instance=RequestContext(request))
 iframe_form = permission_required('imaging.upload_images')(iframe_form)
 
 def ajax_image_removal(request):
-  if request.method == 'POST':
-    image_id = request.POST.get('id', '0')
-    if image_id > 0:
-      try:
-        image = Image.objects.get(pk=image_id)
-        image.delete()
-      except:
-        return HttpResponse("fail");
-      return HttpResponse("ok");
+    if request.method == 'POST':
+        image_id = request.POST.get('id', '0')
+        if image_id > 0:
+            try:
+                image = Image.objects.get(pk=image_id)
+                image.delete()
+            except:
+                return HttpResponse("fail");
+            return HttpResponse("ok");
+        else:
+            return HttpResponse("fail");
     else:
-      return HttpResponse("fail");
-  else:
-    return HttpResponse("fail");
+        return HttpResponse("fail");
 
 ajax_image_removal = permission_required('imaging.ajax_delete_images')(ajax_image_removal)
