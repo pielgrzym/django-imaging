5b81a8c578e0894be3545fb164c2631862512c5f
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-06-09 13:23:47 +0200

    Gallery form handling

--- imaging/views.py
+++ imaging/views.py
@@ -3,6 +3,7 @@ from django.forms.models import modelform_factory, ModelForm
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.utils import simplejson
 
 def gallery_iframe_form(request, modelname):
     from imaging import galleries
@@ -11,6 +12,22 @@ def gallery_iframe_form(request, modelname):
         form = modelform_factory(model, model.get_form() or ModelForm)
     else:
         raise Http404()
-    return render_to_response('imaging/iframe_form.html',
-            { 'form' : form },
-            context_instance=RequestContext(request))
+    callback = None
+    if request.method == 'POST':
+        form = form(request.POST, request.FILES)
+        if form.is_valid():
+            new_item = form.save()
+            response_dict = {
+                    'title':"Image",
+                    'id'     :new_item.pk,
+                    'image':new_item.imaging_thumbnail.url,
+                    }
+            callback = simplejson.dumps(response_dict)
+            form = form()
+    else:
+        form = form()
+    return render_to_response('imaging/iframe_form.html', {
+        'form' : form ,
+        'callback': callback
+        },
+        context_instance=RequestContext(request))
