337a1f2ca54041ca61d4a92300c0a6a947a573fb
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-06-09 10:09:19 +0200

    Ajaximageselection widget uses render_to_string

--- example_project/gallery/admin.py
+++ example_project/gallery/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from gallery.models import Gallery
+from gallery.models import Gallery, SchoolClass
 
 class GalleryAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 admin.site.register(Gallery, GalleryAdmin)
+
+class SchoolClassAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(SchoolClass, SchoolClassAdmin)
--- imaging/widgets.py
+++ imaging/widgets.py
@@ -26,18 +26,9 @@ class AjaxImageSelection(Widget):
       initial_values = Image.objects.filter(pk__in=ids)
     value = force_unicode(value)
     final_attrs = self.build_attrs(attrs, name=name)
-    images_html = ''
-    if len(initial_values) > 0:
-      for image in initial_values:
-        images_html += '<div class="image_block"><span class="image_id">'+str(image.pk)+'</span><a href="/admin/imaging/image/'+str(image.pk)+'/" target="_blank" class="edit_image">Edit</a><a href="#" class="delete_image">Delete</a><img src="'+image.imaging_thumbnail.url+'" alt="'+image.alt+'" /><br /><span class="image_name">'+image.name[:25]+'</span></div>'
-
-    html = '''
-    <div class="image_container"> %s </div>
-
-    <div class="iframe_container">
-      <iframe src="/imaging/iframe_form/" class="image_upload_form" width="100%%" height="205" frameborder="0"></iframe>
-    </div>
-    ''' % images_html
-
-    return mark_safe(u'<input type="hidden" class="imaging_data" %s value="%s" /> %s' % (flatatt(final_attrs),
-                conditional_escape(force_unicode(value)), html))
+    template = render_to_string("imaging/admin_widget.html",{
+        'existing_images': initial_values,
+        'attrs': flatatt(final_attrs),
+        'value': conditional_escape(force_unicode(value)),
+        })
+    return mark_safe(template)
